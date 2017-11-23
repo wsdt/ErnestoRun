dbe3ee9bd9539b61394dbd6310257219263e6a9c
kevin <kevin.riedl.privat@gmail.com>
2017-11-23 11:57:09 +0100

    initial

---
+++ app/src/main/java/fhkufstein/ac/at/ernestorun/MainActivity.java
@@ -0,0 +1,13 @@
+package fhkufstein.ac.at.ernestorun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
