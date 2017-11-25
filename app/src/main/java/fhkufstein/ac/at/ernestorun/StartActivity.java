b602ffb2b370f235b3efbc1ac6dc4ba173eb4ff4
pgsaller <philipp.gsaller@gmail.com>
2017-11-25 12:33:16 +0100

    character

--- app/src/main/java/fhkufstein/ac/at/ernestorun/StartActivity.java
+++ app/src/main/java/fhkufstein/ac/at/ernestorun/StartActivity.java
@@ -17,7 +17,9 @@ public class StartActivity extends AppCompatActivity implements View.OnClickList
 
         findViewById(R.id.snail).setOnClickListener(this);
         findViewById(R.id.raupe).setOnClickListener(this);
+        findViewById(R.id.duck).setOnClickListener(this);
         findViewById(R.id.turtle).setOnClickListener(this);
+        findViewById(R.id.chick).setOnClickListener(this);
         getSupportActionBar().hide();
 
         ImageButton snailAnimation = (ImageButton) findViewById(R.id.snail);
@@ -26,15 +28,25 @@ public class StartActivity extends AppCompatActivity implements View.OnClickList
         ImageButton raupeAnimation = (ImageButton) findViewById(R.id.raupe);
         ((AnimationDrawable) raupeAnimation.getBackground()).start();
 
+        ImageButton duckAnimation = (ImageButton) findViewById(R.id.duck);
+        ((AnimationDrawable) duckAnimation.getBackground()).start();
+
         ImageButton turtleAnimation = (ImageButton) findViewById(R.id.turtle);
         ((AnimationDrawable) turtleAnimation.getBackground()).start();
+
+        ImageButton chickAnimation = (ImageButton) findViewById(R.id.chick);
+        ((AnimationDrawable) chickAnimation.getBackground()).start();
 
     }
 
     @Override
     public void onClick(View view) {
         Intent i = new Intent(this, GameActivity.class);
-        i.putExtra("character", R.id.snail == view.getId() ? R.drawable.snail : (R.id.raupe == view.getId() ? R.drawable.raupe : R.drawable.turtle));
+        i.putExtra("character",
+                view.getId()==R.id.snail ? R.drawable.snail :
+                view.getId()==R.id.raupe ? R.drawable.raupe :
+                        view.getId()==R.id.duck ? R.drawable.duck :
+                                view.getId()==R.id.turtle ? R.drawable.turtle : R.drawable.chick);
         startActivity(i);
     }
 }
